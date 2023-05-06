dc76700115e1c2ab3d2f63ced112aec9dc0be5f7
MarianFIUBA <masuarez@fi.uba.ar>
2023-05-06 19:41:02 +0000

    ficha terminada y celda corregida

--- celda.h
+++ celda.h
@@ -8,29 +8,35 @@ class Celda {
     private:
         TipoCelda tipo;
         TipoContenido contenido;
-        int jugadorOwner;
-    
+        bool estado;
+
     public:
 
-        Celda();
+        Celda(TipoCelda tipoCelda);
 
         TipoCelda getTipo();
 
         TipoContenido getEntidad();
 
-        int getJugadorOwner();
-
         void setTipo(TipoCelda tipo);
 
         void setEntidad(TipoContenido entidad);
 
-        void setJugadorOwner(int jugadorOwner);
+        void setEstado(bool estado);
+
+        bool getEstado();
 };
 
-Celda::Celda() {
-    this->tipo = AIRE;
-    this->contenido = VACIO;
-    this->jugadorOwner = -1;
+Celda::Celda(TipoCelda tipoCelda) {
+    this->tipo = tipoCelda;
+}
+
+bool Celda::getEstado() {
+    return this->estado;
+}
+
+void Celda::setEstado(bool estado) {
+    this->estado = estado;
 }
 
 TipoCelda Celda::getTipo() {
@@ -41,20 +47,12 @@ TipoContenido Celda::getEntidad() {
     return this->contenido;
 }
 
-int Celda::getJugadorOwner() {
-    return this->jugadorOwner;
-}
-
 void Celda::setTipo(TipoCelda tipo) {
     this->tipo = tipo;
 }
 
 void Celda::setEntidad(TipoContenido entidad) {
     this->contenido = entidad;
-}
-
-void Celda::setJugadorOwner(int jugadorOwner) {
-    this->jugadorOwner = jugadorOwner;
 }
 
 #endif
--- ficha.h
+++ ficha.h
@@ -1,35 +1,31 @@
 #include <string>
 
+enum TipoContenido {SOLDADO, ARMAMENTO, MINA, CARTA, VACIO};
+
 class ficha{
     private:
-        std::string tipo;
-        bool activa;
-        int coorX, coorY, coorZ;
+        int jugadorOwner;
+        std::string tipo; // revisar los enums y cambiar 
     public:
         ficha();
-        void setCoords(int x, int y, int z);
         void setTipo(std::string nombre);
-        void setActiva(bool estado);
-        int getCoords(char coordenada);
         std::string getTipo();
-        bool getActiva();
+        int getJugadorOwner();
+        void setJugadorOwner(int jugadorOwner);
 };
 
 // Constructor y destructor en las siguietes dos lineas
 ficha::ficha() {
     this->tipo = "";
-    this->activa = true;
-    this->coorX = 0;
-    this->coorY = 0;
-    this->coorZ = 0;
+    this->jugadorOwner = NULL;
 }
 
-// Pre: recibe un número entero por parámetro, si o si un valor para cada uno. No se puede poner solo un parámetro.
-// Post: dcambia los valores de la coordenadas en los atributos. 
-void ficha::setCoords(int x, int y, int z) {
-    this->coorX = x;
-    this->coorY = y;
-    this->coorZ = z;
+int ficha::getJugadorOwner() {
+    return this->jugadorOwner;
+}
+
+void ficha::setJugadorOwner(int jugadorOwner) {
+    this->jugadorOwner = jugadorOwner;
 }
 
 // Pre: recibe el nombre que va a recibir el nombre de la ficha, este es un string.
@@ -38,27 +34,7 @@ void ficha::setTipo(std::string nombre) {
     this->tipo = nombre;
 }
 
-// Pre: recibe un bool sin más.
-// Post: cambia el bool del atributo 'activa'.
-void ficha::setActiva(bool estado) {
-    this->activa = estado;
-}
-
-// Pre: recibe la coordenada que se quiere obtener x,y,z respectivamente como caracteres.
-// Post: devuelve la coordenada pedida. 
-int ficha::getCoords(char coordenada) {
-    if (coordenada == 'x') return this->coorX;
-    else if (coordenada == 'y') return this->coorY;
-    else if (coordenada == 'z') return this->coorZ;
-    return -1;
-}
-
 // Post: devuelve el contenido del atributo 'tipo'.
 std::string ficha::getTipo() {
     return this->tipo;
-}
-
-// Post: devuelve el contenido del atributo 'activa'.
-bool ficha::getActiva() {
-    return this->activa;
 }
