92604212606270a01e3fb7b2e80a8797b72e54b2
MarianFIUBA <masuarez@fi.uba.ar>
2023-05-07 22:59:51 +0000

    push moszpi

--- ficha.h
+++ ficha.h
@@ -1,88 +1,40 @@
 #include <string>
 
+enum TipoContenido {SOLDADO, ARMAMENTO, MINA, CARTA, VACIO};
+
 class ficha{
     private:
-        std::string* tipo;
-        bool* activa;
-        int* coorX, *coorY, *coorZ;
-        int* vida;
+        int jugadorOwner;
+        std::string tipo; // revisar los enums y cambiar 
     public:
         ficha();
-        ~ficha();
-        void setCoords(int x, int y, int z);
         void setTipo(std::string nombre);
-        void setActiva(bool estado);
-        void setVida(int danio);
-        int getCoords(char coordenada);
         std::string getTipo();
-        bool getActiva();
-        int getVida();
+        int getJugadorOwner();
+        void setJugadorOwner(int jugadorOwner);
 };
 
 // Constructor y destructor en las siguietes dos lineas
 ficha::ficha() {
-    this->tipo = new std::string("");
-    this->activa = new bool(true);
-    this->coorX = new int(0);
-    this->coorY = new int(0);
-    this->coorZ = new int(0);
-    this->vida = new int(0);
-}
-ficha::~ficha() {
-    delete tipo;
-    delete activa;
-    delete coorX;
-    delete coorY;
-    delete coorZ;    
-    delete vida;
+    this->tipo = "";
+    this->jugadorOwner = NULL;
 }
 
-// Pre: recibe un número entero por parámetro, si o si un valor para cada uno. No se puede poner solo un parámetro.
-// Post: dcambia los valores de la coordenadas en los atributos. 
-void ficha::setCoords(int x, int y, int z) {
-    *coorX = x;
-    *coorY = y;
-    *coorZ = z;
+int ficha::getJugadorOwner() {
+    return this->jugadorOwner;
+}
+
+void ficha::setJugadorOwner(int jugadorOwner) {
+    this->jugadorOwner = jugadorOwner;
 }
 
 // Pre: recibe el nombre que va a recibir el nombre de la ficha, este es un string.
 // Post: cambia el contenido del atributo 'tipo'
 void ficha::setTipo(std::string nombre) {
-    *tipo = nombre;
-}
-
-// Pre: recibe un bool sin más.
-// Post: cambia el bool del atributo 'activa'.
-void ficha::setActiva(bool estado) {
-    *activa = estado;
-}
-
-// Pre: recibe un valor entero.
-// Post: cambia el valor del atributo 'vida' mediante una resta de 'vida' con el valor ingresado en el parámetro.
-void ficha::setVida(int danio) {
-    *vida -= danio;
-}
-
-// Pre: recibe la coordenada que se quiere obtener x,y,z respectivamente como caracteres.
-// Post: devuelve la coordenada pedida. 
-int ficha::getCoords(char coordenada) {
-    if (coordenada == 'x') return *coorX;
-    else if (coordenada == 'y') return *coorY;
-    else if (coordenada == 'z') return *coorZ;
-    return -1;
+    this->tipo = nombre;
 }
 
 // Post: devuelve el contenido del atributo 'tipo'.
 std::string ficha::getTipo() {
-    return *tipo;
-}
-
-// Post: devuelve el contenido del atributo 'activa'.
-bool ficha::getActiva() {
-    return *activa;
-}
-
-// Post: devuelve el contenido del atributo 'vida'.
-int ficha::getVida() {
-    return *vida;
+    return this->tipo;
 }
--- test.cpp
+++ test.cpp
@@ -9,3 +9,30 @@ int main(){
     delete batallaDigital;
     return 0;
 }
+// #include "tablero.h"
+// #include "celda.h"
+// #include <typeinfo>
+// #include <iostream>
+
+// typedef Tablero<Celda*> Juego;
+
+// void cargarTablero(Juego& batallaDigital){
+//     for (int i = 0; i < 5; i++) {
+//         for (int j = 0; j < 5; j++) {
+//             batallaDigital.getTData(i,j,0)->setTipo(TIERRA);
+//         }
+//     }
+//     for (int i = 0; i < 5; i++) {
+//         for (int j = 0; j < 5; j++) {
+//             std::cout << batallaDigital.getTData(i,j,0)->getTipo();
+//         }
+//         std::cout << std::endl;
+//     }
+// }
+
+// int main() {
+//     Juego* batallaDigital = new Juego(5, 5, 5);
+//     cargarTablero(*batallaDigital);
+//     delete batallaDigital;
+//     return 0;
+// }
