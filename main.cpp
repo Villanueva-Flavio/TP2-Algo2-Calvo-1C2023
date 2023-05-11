e8f011757819f1d6d80794c53d7c24fcf7af4e5c
Villanueva-Flavio <fvillanueva@fi.uba.ar>
2023-05-11 17:45:05 -0300

    main.cpp edit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "tablero.h"
+#include "celda.h"
+#include <typeinfo>
+#include <iostream>
+
+using namespace std;
+typedef Tablero<Celda*> Mapa;
+
+void cargarPlaya(Mapa* batallaDigital){
+    for(int i = 0; i < batallaDigital->getTamanioX(); i++){
+        for(int j = 0; j < batallaDigital->getTamanioY(); j++){
+            for(int k = 0; k < batallaDigital->getTamanioZ(); k++){
+                batallaDigital->getTData(i, j, k)->setTipo(k > (batallaDigital->getTamanioZ() / 2)? AIRE : (batallaDigital->getTamanioX() < batallaDigital->getTamanioZ() - 4) ? AGUA : TIERRA);
+            }
+        }
+    }
+}
+
+int main(){
+    Mapa* batallaDigital = new Mapa(20, 20, 20);
+    cargarPlaya(batallaDigital);
+    delete batallaDigital;
+    return 0;
+}
