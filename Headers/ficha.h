e675929d71b15bee95cd9106beac01e56472bc6d
MarianFIUBA <masuarez@fi.uba.ar>
2023-05-23 13:41:53 +0000

    backup

--- Headers/ficha.h
+++ Headers/ficha.h
@@ -1,11 +1,12 @@
 #ifndef __FICHA_H__
 #define __FICHA_H__
 
-enum TipoContenido {SOLDADO, TANQUE, SUBMARINO, AVION, MINA_FICHA, CARTA, VACIO};
+enum TipoContenido {SOLDADO, TANQUE, BARCO, SUBMARINO, AVION, MINA_FICHA, CARTA, VACIO};
 
 class Ficha{
     private:
         int jugadorOwner;
+        int enumerador;
         TipoContenido tipo; 
     public:
         Ficha();
--- main.cpp
+++ main.cpp
@@ -1,36 +1,85 @@
 //hacer sleeps y marcar prints
-#include "./Headers/tablero.h"
-#include "./Headers/celda.h"
-#include "./Headers/renderizador.h"
-#include <typeinfo>
+#include "../Headers/tablero.h"
+#include "../Headers/celda.h"
+#include "../Headers/renderizador.h"
 #include <iostream>
-#include <unistd.h>
-
 using namespace std;
 
-void cargarPlaya(Mapa* batallaDigital){
-    for(int i = 0; i < batallaDigital->getTamanioX(); i++){
-        for(int j = 0; j < batallaDigital->getTamanioY(); j++){
-            for(int k = 0; k < batallaDigital->getTamanioZ(); k++){
-                if(i == 0 || i == batallaDigital->getTamanioX() - 1 || j == 0 || j == batallaDigital->getTamanioY() - 1){
-                    batallaDigital->getTData(i, j, k)->setTipo(CAPA_AGUA);
+void mostrarJuego(Coordenada imgSize, BMP imagen, Mapa* tablero) {
+    imprimirAngulo(imgSize, &imagen, tablero, getMap());
+    imagen.WriteToFile("Partida.bmp");
+}
+1
+void cargarPlaya(Mapa* tablero){
+    for(int i = 0; i < tablero->getTamanioX(); i++){
+        for(int j = 0; j < tablero->getTamanioY(); j++){
+            tablero->getTData(i, j, 0)->setTipo(CAPA_TIERRA);
+        }
+    }
+    tablero->getTData(4,4,1)->getFicha().setTipo(SOLDADO); // Cargo por default un soldado
+}
+
+string pedirOpcion() {
+    string opcion;
+    cout << "Escriba el tipo de ficha que desea seleccionar:\n-avion\n-soldado\n-tanque\n-barco\n-submarino\n";
+    cin >> opcion;
+    return opcion;
+}
+
+bool seEncuentraLaFicha(Mapa* tablero, string opcion, int x, int y, int z) {
+    bool seEncontro = false;
+    if (opcion == "soldado"){
+        seEncontro = (tablero->getTData(x,y,z)->getFicha().getTipo() == SOLDADO) ? true : false ;
+    }
+    else if (opcion == "tanque"){
+        seEncontro = (tablero->getTData(x,y,z)->getFicha().getTipo() == TANQUE) ? true : false ;
+    }
+    else if (opcion == "barco"){
+        seEncontro = (tablero->getTData(x,y,z)->getFicha().getTipo() == BARCO) ? true : false ;
+    }
+    else if (opcion == "avion"){
+        seEncontro = (tablero->getTData(x,y,z)->getFicha().getTipo() == AVION) ? true : false ;
+    }
+    else if (opcion == "submarino"){
+        seEncontro = (tablero->getTData(x,y,z)->getFicha().getTipo() == SUBMARINO) ? true : false ;
+    }
+    return seEncontro;
+}
+
+bool buscarFicha(Mapa* tablero){
+    string opcion = pedirOpcion();
+    bool encontrada = false;
+    for (int x = (opcion == "avion") ? 1 : 0; x < tablero->getTamanioX(); x++){
+        for(int y = 0; y < tablero->getTamanioY(); y++){
+            for(int z = 0; z < tablero->getTamanioZ(); z++){
+                // z = (opcion == "soldado" || opcion == "barco" || opcion == "tanque") ? 0 : z ;
+                if (seEncuentraLaFicha(tablero, opcion, x, y, z)){
+                    encontrada = true;
+                    x = tablero->getTamanioX();
+                    y = tablero->getTamanioY();
+                    z = tablero->getTamanioZ();
                 }
+                // z = ((!encontrada) && (x == tablero->getTamanioX()-1) && (y == tablero->getTamanioY()-1)) ? tablero->getTamanioZ() : z ;
             }
         }
     }
+    if (encontrada){
+        cout << "Ficha encontrada\n";
+    }else {"Ficha no se encontró\n"}
+    return encontrada;
 }
 
 int main(){
-    int size = 20;
+    int size = 10;
     Coordenada imgSize = {size*100, size*70};
-    Mapa* batallaDigital = new Mapa(size, size, size);
     BMP imagen;
     imagen.SetSize(imgSize.x, imgSize.y);
-    batallaDigital->getTData(1, 1, 1)->setTipo(CAPA_AGUA);
-    cout << batallaDigital->getTData(1, 1, 1)->getTipo() << " y la capa arena es: " << CAPA_ARENA << endl;
-    cargarPlaya(batallaDigital);
-    imprimirAngulo(imgSize, &imagen, batallaDigital, getMap());
-    imagen.WriteToFile("Partida.bmp");
-    delete batallaDigital;
+    Mapa* tablero = new Mapa(size, size, size);
+    
+    cargarPlaya(tablero);
+    bool sas = buscarFicha(tablero);
+    mostrarJuego(imgSize, imagen, tablero);
+
+    delete tablero;
     return 0;
 }
