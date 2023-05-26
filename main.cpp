bb68fa863d1de230da0a87f484c18bc07079c91f
MarianFIUBA <masuarez@fi.uba.ar>
2023-05-26 06:06:06 +0000

    Implementación mover fichas

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include "./Headers/tablero.h"
 #include "./Headers/celda.h"
+#include "./Headers/renderizador.h"
 #include <iostream>
 using namespace std;
 
+struct coordenadas{int x; int y; int z;};
+
 void cargarMapa(Tablero<Celda*>* tablero){
-    for(int i = 0; i < tablero->getTamanioX(); i++){
-        for(int j = 0; j < tablero->getTamanioY(); j++){
-            tablero->getTData(i, j, 0)->setTipo(CAPA_TIERRA);
+    for(int x = 0; x < tablero->getTamanioX(); x++){
+        for(int y = 0; y < tablero->getTamanioY(); y++){
+            if (x < (tablero->getTamanioX()/1.5)){
+                tablero->getTData(x, y, 0)->setTipo(CAPA_TIERRA);
+                tablero->getTData(x, y, 1)->setTipo(CAPA_PASTO);
+            }else {
+                tablero->getTData(x, y, 0)->setTipo(CAPA_AGUA);
+                tablero->getTData(x, y, 1)->setTipo(CAPA_AGUA);
+            }
         }
     }
-    tablero->getTData(0,0,0)->getFicha()->setTipo(SOLDADO);
+    tablero->getTData(2,2,1)->getFicha()->setTipo(SOLDADO);
+    tablero->getTData(2,2,1)->getFicha()->setNumFicha(1);
+    tablero->getTData(2,2,1)->setTipo(CAPA_ARENA);
 }
 
 string pedirOpcion() {
@@ -19,58 +30,128 @@ string pedirOpcion() {
     return opcion;
 }
 
-bool seEncuentraLaFicha(Tablero<Celda*>* tablero, string opcion, int x, int y, int z) {
-    bool seEncontro = false;
-    if (opcion == "soldado"){
-        seEncontro = (tablero->getTData(x,y,z)->getFicha()->getTipo() == SOLDADO) ? true : false ;
-    }
-    else if (opcion == "tanque"){
-        seEncontro = (tablero->getTData(x,y,z)->getFicha()->getTipo() == TANQUE) ? true : false ;
-    }
-    else if (opcion == "barco"){
-        seEncontro = (tablero->getTData(x,y,z)->getFicha()->getTipo() == BARCO) ? true : false ;
-    }
-    else if (opcion == "avion"){
-        seEncontro = (tablero->getTData(x,y,z)->getFicha()->getTipo() == AVION) ? true : false ;
-    }
-    else if (opcion == "submarino"){
-        seEncontro = (tablero->getTData(x,y,z)->getFicha()->getTipo() == SUBMARINO) ? true : false ;
-    }
-    return seEncontro;
+int pedirEnumeracion() {
+    int opcion;
+    cout << "Ahora por favor elija el número de ficha que quiere buscar, esto para diferenciarla de las otras.\n-";
+    cin >> opcion;
+    return opcion;
 }
 
-bool buscarFicha(Tablero<Celda*>* tablero){
+bool seEncuentraLaFicha(Tablero<Celda*>* tablero, string opcion, int enumeracionFicha, int x, int y, int z) {
+    bool seEncontro = false;
+    if (opcion == "soldado"){
+        seEncontro = ((tablero->getTData(x,y,z)->getFicha()->getTipo() == SOLDADO) && (tablero->getTData(x,y,z)->getFicha()->getNumFicha() == enumeracionFicha)) ? true : false ;
+    }
+    else if (opcion == "tanque"){
+        seEncontro = ((tablero->getTData(x,y,z)->getFicha()->getTipo() == TANQUE) && (tablero->getTData(x,y,z)->getFicha()->getNumFicha() == enumeracionFicha)) ? true : false ;
+    }
+    else if (opcion == "barco"){
+        seEncontro = ((tablero->getTData(x,y,z)->getFicha()->getTipo() == BARCO) && (tablero->getTData(x,y,z)->getFicha()->getNumFicha() == enumeracionFicha)) ? true : false ;
+    }
+    else if (opcion == "avion"){
+        seEncontro = ((tablero->getTData(x,y,z)->getFicha()->getTipo() == AVION) && (tablero->getTData(x,y,z)->getFicha()->getNumFicha() == enumeracionFicha)) ? true : false ;
+    }
+    else if (opcion == "submarino"){
+        seEncontro = ((tablero->getTData(x,y,z)->getFicha()->getTipo() == SUBMARINO) && (tablero->getTData(x,y,z)->getFicha()->getNumFicha() == enumeracionFicha)) ? true : false ;
+    }
+    return seEncontro; 
+}
+ 
+// Faltan refinar algunas cosas de busqueda
+void buscarCoordenadasFicha(coordenadas* coorFicha, Tablero<Celda*>* tablero){
     string tipoFicha = pedirOpcion();
-    int enumeracionFicha = stoi(pedirOpcion());
+    int enumeracionFicha = pedirEnumeracion();
     int nivelX = 1;
     int nivelZ = 1;
-    bool encontrada = false;
     for (int x = (tipoFicha == "avion") ? nivelX : 0; x < tablero->getTamanioX(); x++){ // Se inicializa x siempre en el nivel del aire si es que se eligió buscar una cosa de ahí
         for(int y = 0; y < tablero->getTamanioY(); y++){
             for(int z = 0; z < tablero->getTamanioZ(); z++){
-                if (seEncuentraLaFicha(tablero,tipoFicha,x,y,z)){
-                    encontrada = true;
+                if (seEncuentraLaFicha(tablero,tipoFicha,enumeracionFicha,x,y,z)){
+                    *coorFicha = {x,y,z};
                     x = tablero->getTamanioX();
                     y = tablero->getTamanioY();
                     z = tablero->getTamanioZ();
                 }
-                z = (z >= nivelZ) ? tablero->getTamanioZ() : z; // Se corta la iteración de nivel si z vale el nivel límite o por encima de tierra
+                // z = (z >= nivelZ) ? tablero->getTamanioZ() : z; // Se corta la iteración de nivel si z vale el nivel límite o por encima de tierra
             }
         }
     }
-    return encontrada;
+}
+
+void pedirMovimiento(char* movimiento){
+    cout << "Ingrese el movimiento:\n-Frontal y tracero: W y S\nLaterales: A y D\n[ ";
+    cin >> *movimiento;
+}
+
+void moverLongitudinalmente(char movimiento, Tablero<Celda*>* tablero, coordenadas fichaActual){
+    Celda auxiliar;
+    if (movimiento == 'W'){
+        if ((fichaActual.x + 1) < tablero->getTamanioX()){
+            auxiliar = *tablero->getTData((fichaActual.x + 1),(fichaActual.y),(fichaActual.z));
+            *tablero->getTData((fichaActual.x + 1),(fichaActual.y),(fichaActual.z)) = *tablero->getTData((fichaActual.x),(fichaActual.y),(fichaActual.z)); 
+            *tablero->getTData((fichaActual.x),(fichaActual.y),(fichaActual.z)) = auxiliar; 
+        }
+    }
+    else if (movimiento == 'S'){
+        if (fichaActual.x-- > 0){
+            auxiliar = *tablero->getTData((fichaActual.x - 1),(fichaActual.y),(fichaActual.z));
+            *tablero->getTData((fichaActual.x - 1),(fichaActual.y),(fichaActual.z)) = *tablero->getTData((fichaActual.x),(fichaActual.y),(fichaActual.z)); 
+            *tablero->getTData((fichaActual.x),(fichaActual.y),(fichaActual.z)) = auxiliar; 
+        }
+    }
+}
+
+void moverLateralmente(char movimiento, Tablero<Celda*>* tablero, coordenadas fichaActual){
+    Celda auxiliar;
+    if (movimiento == 'D'){
+        if ((fichaActual.y + 1) < tablero->getTamanioY()){
+            auxiliar = *tablero->getTData((fichaActual.x),(fichaActual.y + 1),(fichaActual.z)); 
+            *tablero->getTData((fichaActual.x),(fichaActual.y + 1),(fichaActual.z)) = *tablero->getTData((fichaActual.x),(fichaActual.y),(fichaActual.z));
+            *tablero->getTData((fichaActual.x),(fichaActual.y),(fichaActual.z)) = auxiliar;
+        }
+    }
+    else if (movimiento == 'A'){
+        if ((fichaActual.y - 1) > 0){
+            auxiliar = *tablero->getTData((fichaActual.x),(fichaActual.y - 1),(fichaActual.z)); 
+            *tablero->getTData((fichaActual.x),(fichaActual.y - 1),(fichaActual.z)) = *tablero->getTData((fichaActual.x),(fichaActual.y),(fichaActual.z));
+            *tablero->getTData((fichaActual.x),(fichaActual.y),(fichaActual.z)) = auxiliar;
+        }      
+    }
+}
+
+void moverFicha(Tablero<Celda*>* tablero) {
+    char movimiento;
+    coordenadas fichaActual;
+    buscarCoordenadasFicha(&fichaActual,tablero);
+    pedirMovimiento(&movimiento);
+    moverLongitudinalmente(movimiento,tablero,fichaActual);
+    moverLateralmente(movimiento,tablero,fichaActual);
+    fichaActual = fichaActual;
 }
 
 int main(){
-    int size = 10;
+    int size = 20;
+    Coordenada imgSize = {size*100, size*70};
+    BMP imagen;
+    imagen.SetSize(imgSize.x, imgSize.y);
+    
     Tablero<Celda*>* tablero = new Tablero<Celda*>(size, size, size);
     cargarMapa(tablero);
-    if (buscarFicha(tablero)){
-        cout << "¡Encontrada!\n";
-    }
-    else {
-        cout << "¡Inexistente!\n";
-    }
+
+    
+    imprimirAngulo(imgSize, &imagen, tablero, getMap());
+    imagen.WriteToFile("Partida.bmp");
+
+    cout << endl << tablero->getTData(2,2,1)->getTipo() << endl;
+    cout << tablero->getTData(3,2,1)->getTipo() << endl;
+    
+    moverFicha(tablero);
+
+    cout << endl << tablero->getTData(2,2,1)->getTipo() << endl;
+    cout << tablero->getTData(3,2,1)->getTipo() << endl;
+    imprimirAngulo(imgSize, &imagen, tablero, getMap());
+    imagen.WriteToFile("Partida.bmp");
+
     delete tablero;
     return 0;
 }
