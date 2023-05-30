d7406faf4fe4bf9ebabe9fdcfc98083ec88ff680
Gero-F <gfanti@fi.uba.ar>
2023-05-30 17:28:15 -0300

    local changes

--- main.cpp
+++ main.cpp
@@ -13,38 +13,50 @@ const char MOV_IZQUIERDA = 'a';
 const char MOV_ABAJO = 's';
 const char MOV_DERECHA = 'd';
 
-void moverJugador(Mapa* batallaDigital){
+void moverJugador(Mapa* batallaDigital, Coordenada posSoldado){
 
     char movimiento = ' ';
+    int jugadorActual = -1;
 
     cout << "Ingrese un movimiento: \n";
     cin >> movimiento;
 
     while(movimiento != MOV_ABAJO && movimiento != MOV_ARRIBA && movimiento != MOV_DERECHA && movimiento != MOV_IZQUIERDA){
 
-        cout << "\nEntra al while";            //BORRAR
         cout << "\nIngresa un movimientos valido (W-A-S-D): ";
         cin >> movimiento;
+        //AGREGAR VALIDACION DE MOVIMIENTO
     }
 
     cout << "Mov ingresado: " << movimiento;
 
-    batallaDigital->getTData(5,5,2)->getFicha()->setTipo(VACIO);
-    batallaDigital->getTData(5,5,2)->getFicha()->setJugadorOwner(-1);     //
+    batallaDigital->getTData(posSoldado.x, posSoldado.y, posSoldado.z)->getFicha()->setTipo(VACIO);
+    jugadorActual = batallaDigital->getTData(posSoldado.x, posSoldado.y, posSoldado.z)->getFicha()->getJugadorOwner();
+    batallaDigital->getTData(posSoldado.x, posSoldado.y, posSoldado.z)->getFicha()->setJugadorOwner(-1);
 
 
-    if(movimiento == MOV_ARRIBA){
+    if(movimiento == MOV_ARRIBA){           //validacion de q no se salga (getsize)
 
-        batallaDigital->getTData(5,5 +1,2)->getFicha()->setTipo(SOLDADO);
-        cout << "ASDASDAS\n";
-        batallaDigital->getTData(5,5 +1,2)->getFicha()->setJugadorOwner(1);         //
+        batallaDigital->getTData(posSoldado.x, posSoldado.y +1, posSoldado.z)->getFicha()->setTipo(SOLDADO);    //acomodar: el tipo de ficha se tiene q pasar por argumento
+        batallaDigital->getTData(posSoldado.x, posSoldado.y +1, posSoldado.z)->getFicha()->setJugadorOwner(jugadorActual);
     }
     else if(movimiento == MOV_ABAJO){
 
-        cout << "S\n";
+        batallaDigital->getTData(posSoldado.x, posSoldado.y -1, posSoldado.z)->getFicha()->setTipo(SOLDADO);
+        batallaDigital->getTData(posSoldado.x, posSoldado.y -1, posSoldado.z)->getFicha()->setJugadorOwner(jugadorActual);
+    }
+    else if(movimiento == MOV_DERECHA){
+
+        batallaDigital->getTData(posSoldado.x +1, posSoldado.y, posSoldado.z)->getFicha()->setTipo(SOLDADO);
+        batallaDigital->getTData(posSoldado.x +1, posSoldado.y, posSoldado.z)->getFicha()->setJugadorOwner(jugadorActual);
+    }
+    else{
+
+        batallaDigital->getTData(posSoldado.x -1, posSoldado.y, posSoldado.z)->getFicha()->setTipo(SOLDADO);
+        batallaDigital->getTData(posSoldado.x -1, posSoldado.y, posSoldado.z)->getFicha()->setJugadorOwner(jugadorActual);
     }
 
-    }
+}
 
 void actualizarCeldas(Mapa *batallaDigital){
 
@@ -53,7 +65,7 @@ void actualizarCeldas(Mapa *batallaDigital){
             for(int k = 0; k < batallaDigital->getTamanioZ(); k++){
                 if(batallaDigital->getTData(i, j, k)->getFicha()->getTipo() != VACIO){           // 
                     batallaDigital->getTData(i, j, k)->setTipo(CAPA_ARENA);
-                } else {
+                } else {    //arreglar
                     batallaDigital->getTData(i, j, k)->setTipo(CAPA_AGUA);
                 }
             }
@@ -84,8 +96,13 @@ int main(){
     
     cargarPlaya(batallaDigital);
 
+    Coordenada posSoldado;
+    posSoldado.x = 5;
+    posSoldado.y = 5;
+    posSoldado.z = 2;
+
     // Coloco un soldado en la posicion 5,5,2 para posteriormente moverlo
-    batallaDigital->getTData(5,5,2)->getFicha()->setJugadorOwner(1);
+    batallaDigital->getTData(posSoldado.x, posSoldado.y, posSoldado.z)->getFicha()->setJugadorOwner(1);
     batallaDigital->getTData(5,5,2)->getFicha()->setTipo(SOLDADO);
 
     actualizarCeldas(batallaDigital);
@@ -94,7 +111,7 @@ int main(){
     imagen.WriteToFile("Partida.bmp");
 
     // Muevo al soldado y actualizo los datos
-    moverJugador(batallaDigital);
+    moverJugador(batallaDigital, posSoldado);
     actualizarCeldas(batallaDigital);
 
     // Imprimo el mapa con la nueva posicion del soldado
