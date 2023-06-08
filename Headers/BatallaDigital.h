01b011a023d2c625d5e29aac5bc286539025f29b
Gero-F <gfanti@fi.uba.ar>
2023-06-07 23:29:38 -0300

    TDA BatallaDigital (en proceso)

---
+++ Headers/BatallaDigital.h
@@ -0,0 +1,20 @@
+#ifndef __BATALLA_DIGITAL_H__
+#define __BATALLA_DIGITAL_H__
+
+#include "Tablero.h"
+#include "Jugador.h"
+
+enum EstadoJuego {COMENZADO, FINALIZADO};
+
+void cargarJuego(Tablero<Celda*>* tablero, Lista<Jugador*>* jugadores, std::string* nombres, std::string nombreMundo, int numeroDeJugadores);
+
+void mostrarTerreno(Tablero<Celda*>* tablero, int size);
+
+void actualizarJuego(Tablero<Celda*>* tablero, Lista<Jugador*>* jugadores, int turnoDelJugador);
+
+EstadoJuego estadoDelJuego(Lista<Jugador*>* listaJugadores, int cantJugadores);
+
+
+#include "../TPPs/BatallaDigital.tpp"
+
+#endif  //__BATALLA_DIGITAL_H__
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "./Headers/Tablero.h"
 #include "./Headers/Celda.h"
-#include "./Headers/Renderizador.h"
 #include "./Headers/Jugador.h"
+#include "./Headers/BatallaDigital.h"
 #include "./Headers/DatosIngresados.h"
 #include <iostream>
 #include <string>
@@ -10,56 +10,53 @@ using namespace std;
 
 
 
-void procesarMapa(Tablero<Celda*>* tablero, int size) {
-    Coordenada imgSize = {size*100, size*70};
-    BMP imagen;
-    imagen.SetSize(imgSize.x,imgSize.y);
-    imprimirAngulo(imgSize,(&imagen),tablero, getMap());
-    imagen.WriteToFile("Partida.bmp");
-}
+
 
 int main(){
-    string cortar = "",ficha = "";
-    bool seguir = false;
-    int size = 20;
-    coordenadas coordenadaFichaActual = {-1,-1,-1};
-    Tablero<Celda*>* tablero = new Tablero<Celda*>(size, size, size);
-
-    Lista<Jugador*>* listaJugadores = new Lista<Jugador*>();        //agregado
-    int cantJugadores;
+    Lista<Jugador*>* listaJugadores = new Lista<Jugador*>();
+    int cantJugadores = 0;
+    int turnoDelJugador = 1;
     int r = 0, g = 0, b = 0;
     string tipoDeMapa = "";
+    EstadoJuego estadoBatallaDigital = COMENZADO;
     char movimiento;
     coordenadas ubicacionMina;
 
     solicitarJugadores(&cantJugadores);
+    int mapSize = cantJugadores*4;
 
+    Tablero<Celda*>* tablero = new Tablero<Celda*>(mapSize, mapSize, mapSize);
+    
     string* nombreJugadores = new string[cantJugadores];
     int* valoresR = new int[cantJugadores];
     int* valoresG = new int[cantJugadores];
     int* valoresB = new int[cantJugadores];
 
+
     pedirDatosIniciales(cantJugadores, tipoDeMapa, nombreJugadores, valoresR, valoresG ,valoresB);
+    cargarJuego(tablero, listaJugadores, nombreJugadores, tipoDeMapa, cantJugadores);
 
-    pedirMovimiento(&movimiento);
-    pedirUbicacionMina(&ubicacionMina, size);
-
-    //para probar
-    cout << "\nTipo de mapa ingresado: " << tipoDeMapa;
-    cout << "\nMovimiento: " << movimiento;
-    cout << "\ncoordenadas: " << ubicacionMina.x << ubicacionMina.y << ubicacionMina.z;
-    for(int i=0; i<cantJugadores; i++){
-        cout << "\nnombre jugador " << i+1 << ": " << nombreJugadores[i] << "\n";
-        cout << "colores: " << valoresR[i] << valoresG[i] << valoresB[i];
+    while(estadoBatallaDigital != FINALIZADO){
+        pedirMovimiento(&movimiento);
+        pedirUbicacionMina(&ubicacionMina, mapSize);
+        //actualizarJuego(tablero, listaJugadores, turnoDelJugador);
+        mostrarTerreno(tablero,mapSize);
+        estadoBatallaDigital = estadoDelJuego(listaJugadores, cantJugadores);
+        
+        if(turnoDelJugador != cantJugadores) {
+            turnoDelJugador++;
+        } else {
+            turnoDelJugador = 1;
+        }
     }
 
-    //cargarJuego(listaJugadores);
+
+
 
     delete[] valoresR;
     delete[] valoresG;
     delete[] valoresB;
     delete[] nombreJugadores;
-
     delete tablero;
     delete listaJugadores;
     return 0;
