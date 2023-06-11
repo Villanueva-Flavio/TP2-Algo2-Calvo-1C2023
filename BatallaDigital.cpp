8c67f035022cb889b758b1b166a2ff7bf5180a78
Villanueva-Flavio <fvillanueva@fi.uba.ar>
2023-06-11 18:47:23 -0300

    Orden establecido para BatallaDigital, portear funciones a metodos!

--- BatallaDigital.cpp
+++ BatallaDigital.cpp
@@ -50,11 +50,42 @@ void BatallaDigital::consultarTipoDeMapa(){
 // Gero
 }
 
+int jugadoresConFichas(){
+    
+}
+
 void BatallaDigital::cargarJuego(){
+    //Consultar Nombres | SIN PARAMETROS
+    //Consultar Colores | SIN PARAMETROS
+
     //Consultar tipo de mapa | enum tipo
     //Cargar tipo mapa | enum tipo
     //Posicionar fichas  | enum tipo
 }
 
-void BatallaDigital::
+void BatallaDigital::ejecutarTurno(){
+    for(int i = 0; i < this->cantidadJugadores; i++){
+        
+        //Si jugador no tiene fichas -> no juega
+        //Si jugador tiene turno omitido -> no juega
+        
+        //Print Mapa_Jugador_X.bmp
 
+        //Pedir Ficha a mover - Mostrar ficha
+        //Pedir movimiento o accion
+        //Ejecutar movimiento o accion
+
+        //Soltar mina
+        //Comprobar si explota
+
+        //Robar Carta    
+
+    }
+}
+
+void BatallaDigital::jugar(){
+    while(this->jugadoresConFichas() > 1){
+        this->ejecutarTurno();
+    }
+    //mensajeVictoria(this->jugadores->getJugador(0)->getNombre()); (Podria quedar para main)
+}
